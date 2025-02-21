ae1c7490e4e70670d57a5e399dea425b7a4fe590
Chris Ortega <chrisortega94@gmail.com>
2025-02-20 18:29:26 -0800

    add test json and message_handler

---
+++ page_events/lambda_function.py
@@ -0,0 +1,81 @@
+import json
+import urllib3
+import os
+import logging
+
+def lambda_handler(event, context):
+    logging_handler(event)
+    whurl = os.environ['TESTING'] #for testing channel output
+    #whurl = os.environ['SLACK_WEBHOOK']
+    #message = {"text": f"{event}"} #uncomment to output entire event to slack, may bog down speeds
+    message = message_handler(event)
+    send_webhook(whurl, message)
+
+    #AKA reverse api
+def send_webhook(webhook_url, payload):
+    #text = "Hello Hammer Creative, from AWS Lambda"
+    
+    # Create an HTTP pool manager
+    http = urllib3.PoolManager()
+    try:
+        # Send the POST request with JSON payload
+        response = http.request(
+            "POST",
+            webhook_url,
+            body=json.dumps(payload)
+        )
+        
+        # Check for successful response
+        if response.status != 200:
+            print(f"Error: {response.status}")
+        
+        else:
+            print("Webhook sent successfully")
+
+    except Exception as e:
+        # Log the error if the request fails
+        return {"statusCode": 500, "body": f"Error: {str(e)}"}
+
+#TODO, needs to change based on event type
+def message_handler(event_name):
+    action = ''
+    page_name = ''
+    date_occurred = ''
+    user = ''
+    message = {"text": "----------------------------------------------\n"+
+                f"** {action} ** \n"+
+                f"Page Name: \"{page_name}\" \n"+
+                f"Date: \"{date_occurred}\" \n"+
+                f"By: \"{user}\""}
+
+    if "page_create" == event_name['event']:
+        action = 'PAGE CREATED'
+        page_name = event_name['related_item']['name']
+        date_occurred = event_name['related_item']['created_at']
+        user = event_name['related_item']['created_by']['name']
+
+    if "page_update" == event_name['event']:
+        action = 'PAGE UPDATED'
+        page_name = event_name['related_item']['name']
+        date_occurred = event_name['related_item']['updated_at']
+        user = event_name['related_item']['updated_by']['name']
+
+    if "page_delete" == event_name['event']:
+        action = 'PAGE DELETED'
+        page_name = event_name['related_item']['name']
+        date_occurred = event_name['related_item']['updated_at']
+        user = event_name['related_item']['updated_by']['name']
+
+    if "page_move" == event_name['event']:
+        action = 'PAGE MOVED'
+        page_name = event_name['related_item']['name']
+        date_occurred = event_name['related_item']['updated_at']
+        user = event_name['related_item']['updated_by']['name']
+    
+    return message
+
+def logging_handler(event):
+    logger = logging.getLogger()
+    logger.setLevel("INFO")
+    logger.info(event)     
+ 
